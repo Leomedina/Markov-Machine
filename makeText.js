8bdf8b581a4f0992e03e90ff975946f9325ae969
Leo <medinayleo@gmail.com>
2020-06-24 11:21:47 -0400

    created object that takes apart string into markov objects

--- makeText.js
+++ makeText.js
@@ -1,2 +1,14 @@
 /** Command-line tool to generate Markov text. */
 
+const chainMap = this.words.map((word, index, words) => {
+    const container = {}
+    const values = []
+    const key = word;
+
+    container[key] = values;
+    values.push(this.checkForNull(words, index));
+
+    return container;
+})
+
+console.log(chainMap)
--- markov.js
+++ markov.js
@@ -17,9 +17,28 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    const container = {}
+
+    for (let i = 0; i < this.words.length; i++) {
+      const KEY = this.words[i];
+
+      if (container[KEY]) {
+        container[KEY].push(this.checkForNull(this.words, i))
+      } else {
+        container[KEY] = [];
+        container[KEY].push(this.checkForNull(this.words, i))
+      }
+    }
+    console.log(container)
   }
 
+  checkForNull(words, index) {
+    if (words[index + 1]) {
+      return words[index + 1];
+    } else {
+      return null;
+    }
+  }
 
   /** return random text from chains */
 
@@ -27,3 +46,6 @@ class MarkovMachine {
     // TODO
   }
 }
+
+const markovMachine = new MarkovMachine("the cat in the hat");
+
